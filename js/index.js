5e5d9abaf83b0aa1bdb546dd47e920725c23495a
Nathan Kirby <natekirbz@gmail.com>
2024-12-26 22:33:22 -0500

    set up index and basic sites

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//js doesnt have a main method so no need for public static void "main(String args[])"
+//test func
+function testFunc() {
+    alert("you are going to die in 11 minutes");
+}
+function hover(x) {
+    x.style.background = "#B599CE";
+}
+function notHover(x) {
+    x.style.background = "#CBB677";
+}
+
